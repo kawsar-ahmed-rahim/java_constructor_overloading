4543cec26bed0d6443505453f4359b5c48413840
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-16 09:46:46 +0600

    first commit

---
+++ EmployeeSalary.java
@@ -0,0 +1,19 @@
+// Exercise 1: Employee Salary
+// Create a class Employee.
+// Variables:
+// String name;
+// double salary;
+// double bonus;
+// Constructor overloading:
+// Employee(String name)
+// Employee(String name, double salary)
+// Employee(String name, double salary, double bonus)
+// Method overloading:
+// double calculateSalary()
+// double calculateSalary(double overtime)
+// double calculateSalary(double overtime, double allowance)
+// Rules:
+// calculateSalary()= salary + bonus
+// calculateSalary(overtime)= salary + bonus + overtime
+// calculateSalary(overtime, allowance)= salary + bonus + overtime + allowance
+
---
+++ RectangleMain.java
@@ -0,0 +1,14 @@
+// Exercise 2: Rectangle
+// Create a class Rectangle.
+// Variables:
+// double length;
+// double width;
+// Constructor overloading:
+// Rectangle()
+// Rectangle(double side)
+// Rectangle(double length, double width)
+// Create overloaded methods named calculate():
+// double calculate()
+// double calculate(double length)
+// double calculate(double length, double width)
+
