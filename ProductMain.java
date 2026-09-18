bfc4f922a23a081b39da55c0bff09e83a20cea7d
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-19 02:18:55 +0600

    TILL NOW

---
+++ ProductMain.java
@@ -0,0 +1,19 @@
+// Exercise 4: Product
+// Create a class Product.
+// Variables:
+// String name;
+// double price;
+// int quantity;
+// Constructor overloading:
+// Product(String name)
+// Product(String name, double price)
+// Product(String name, double price, int quantity)
+// Create overloaded calculatePrice() methods:
+// double calculatePrice()
+// double calculatePrice(int quantity)
+// double calculatePrice(int quantity, double discount)
+// Rules:
+// calculatePrice()= price × object quantity
+// calculatePrice(quantity)= price × given quantity
+// calculatePrice(quantity, discount)= total price after percentage discount
+
